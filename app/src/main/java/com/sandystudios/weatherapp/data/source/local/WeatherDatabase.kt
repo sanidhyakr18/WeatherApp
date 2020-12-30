b3894bff42b44d7381b9c26bdd43cc84fb1fb9f2
sanidhyakr18 <sanidhyakr18@gmail.com>
2020-12-30 21:19:53 +0530

    used type converter for list of data

---
+++ app/src/main/java/com/sandystudios/weatherapp/data/source/local/WeatherDatabase.kt
@@ -0,0 +1,17 @@
+package com.sandystudios.weatherapp.data.source.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.sandystudios.weatherapp.data.source.local.dao.WeatherDao
+import com.sandystudios.weatherapp.data.source.local.entity.DBWeather
+import com.sandystudios.weatherapp.data.source.local.entity.DBWeatherForecast
+
+@Database(entities = [DBWeather::class, DBWeatherForecast::class], version = 1, exportSchema = true)
+@TypeConverters(
+    ListNetworkWeatherDescriptionConverter::class
+)
+abstract class WeatherDatabase : RoomDatabase() {
+
+    abstract val weatherDao: WeatherDao
+}
